6f0c1d568a6ae4d68d3b09085bb28ebd5ba3892e
surenot <surenot@debian>
2016-01-05 01:56:17 -0500

    Beautify config file handling

--- site-cookbooks/server-conf/recipes/default.rb
+++ site-cookbooks/server-conf/recipes/default.rb
@@ -27,12 +27,11 @@ directory '/etc/transmission-daemon' do
   action :create
 end
 
-file "/etc/transmission-daemon/settings.json" do
-  # content nginx_conf
-  content "{\n    \"alt-speed-down\": 50, \n    \"alt-speed-enabled\": false, \n    \"alt-speed-time-begin\": 540, \n    \"alt-speed-time-day\": 127, \n    \"alt-speed-time-enabled\": false, \n    \"alt-speed-time-end\": 1020, \n    \"alt-speed-up\": 50, \n    \"bind-address-ipv4\": \"0.0.0.0\", \n    \"bind-address-ipv6\": \"::\", \n    \"blocklist-enabled\": true, \n    \"blocklist-url\": \"http://www.example.com/blocklist\", \n    \"cache-size-mb\": 4, \n    \"dht-enabled\": true, \n    \"download-dir\": \"/var/lib/transmission-daemon/downloads\", \n    \"download-limit\": 100, \n    \"download-limit-enabled\": 0, \n    \"download-queue-enabled\": true, \n    \"download-queue-size\": 5, \n    \"encryption\": 1, \n    \"idle-seeding-limit\": 30, \n    \"idle-seeding-limit-enabled\": false, \n    \"incomplete-dir\": \"/var/lib/transmission-daemon/incomplete\", \n    \"incomplete-dir-enabled\": true, \n    \"lpd-enabled\": false, \n    \"max-peers-global\": 200, \n    \"message-level\": 1, \n    \"peer-congestion-algorithm\": \"\", \n    \"peer-id-ttl-hours\": 6, \n    \"peer-limit-global\": 200, \n    \"peer-limit-per-torrent\": 50, \n    \"peer-port\": 51413, \n    \"peer-port-random-high\": 65535, \n    \"peer-port-random-low\": 49152, \n    \"peer-port-random-on-start\": false, \n    \"peer-socket-tos\": \"lowcost\", \n    \"pex-enabled\": true, \n    \"port-forwarding-enabled\": false, \n    \"preallocation\": 1, \n    \"prefetch-enabled\": 1, \n    \"queue-stalled-enabled\": true, \n    \"queue-stalled-minutes\": 30, \n    \"ratio-limit\": 2, \n    \"ratio-limit-enabled\": true, \n    \"rename-partial-files\": true, \n    \"rpc-authentication-required\": true, \n    \"rpc-bind-address\": \"0.0.0.0\", \n    \"rpc-enabled\": true, \n    \"rpc-password\": \"transmission\", \n    \"rpc-port\": 9091, \n    \"rpc-url\": \"/transmission/\", \n    \"rpc-username\": \"transmission\", \n    \"rpc-whitelist\": \"*\", \n    \"rpc-whitelist-enabled\": true, \n    \"scrape-paused-torrents-enabled\": true, \n    \"script-torrent-done-enabled\": false, \n    \"script-torrent-done-filename\": \"\", \n    \"seed-queue-enabled\": false, \n    \"seed-queue-size\": 10, \n    \"speed-limit-down\": 100, \n    \"speed-limit-down-enabled\": false, \n    \"speed-limit-up\": 100, \n    \"speed-limit-up-enabled\": false, \n    \"start-added-torrents\": true, \n    \"trash-original-torrent-files\": false, \n    \"umask\": 18, \n    \"upload-limit\": 100, \n    \"upload-limit-enabled\": 0, \n    \"upload-slots-per-torrent\": 14, \n    \"utp-enabled\": true\n}"
+cookbook_file "/etc/transmission-daemon/settings.json" do
+  source 'transmission/settings.json'
   owner 'root'
   group 'root'
-  mode '0755'
+  mode '0744'
 end
 
 docker_image 'dperson/transmission' do
@@ -55,7 +54,6 @@ end
 # --------------------
 # nginx setup
 # --------------------
-# nginx_conf = data_bag_item('nginx-conf', default)['content']
 directory '/etc/nginx/conf.d' do
   recursive true
   owner 'root'
@@ -64,22 +62,19 @@ directory '/etc/nginx/conf.d' do
   action :create
 end
 
-file "/etc/nginx/conf.d/default.conf" do
-  # content nginx_conf
-  content "server {\n    listen       80;\n    server_name  transmission.surenot.ml;\n\n    location / {\n        proxy_pass http://transmission:9091;\n    }\n}\n\nserver {\n    listen       80;\n    server_name  localhost surenot.ml www.surenot.ml;\n\n    location / {\n        root   /usr/share/nginx/html;\n        index  index.html index.htm;\n    }\n}"
+cookbook_file "/etc/nginx/conf.d/default.conf" do
+  source 'nginx/default.conf'
   owner 'root'
   group 'root'
-  mode '0755'
+  mode '0744'
 end
 
-# Pull latest image
 docker_image 'nginx' do
   tag 'latest'
   action :pull
   notifies :redeploy, 'docker_container[my_nginx]'
 end
 
-# Run nginx container on port 80
 docker_container 'my_nginx' do
   repo 'nginx'
   tag 'latest'
